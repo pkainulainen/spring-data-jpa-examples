ec7bc7516fb9929201a5a0cfb451ef3f3b95f2f0
Petri Kainulainen <petri.kainulainen@gmail.com>
2012-02-28 22:02:20 +0200

    Added comments and fixed a bug in the PersonPredicates class

--- Spring/data-jpa/tutorial-part-five/src/main/java/net/petrikainulainen/spring/datajpa/repository/PersonPredicates.java
+++ Spring/data-jpa/tutorial-part-five/src/main/java/net/petrikainulainen/spring/datajpa/repository/PersonPredicates.java
@@ -1,14 +1,7 @@
 package net.petrikainulainen.spring.datajpa.repository;
 
-import com.mysema.query.types.expr.BooleanExpression;
-import net.petrikainulainen.spring.datajpa.model.Person;
+import com.mysema.query.types.Predicate;
 import net.petrikainulainen.spring.datajpa.model.QPerson;
-import org.springframework.data.jpa.domain.Specification;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
 /**
  * A class which is used to create Querydsl predicates.
@@ -16,9 +9,9 @@ import javax.persistence.criteria.Root;
  */
 public class PersonPredicates {
 
-    public static BooleanExpression lastNameIsLike(final String searchTerm) {
+    public static Predicate lastNameIsLike(final String searchTerm) {
         QPerson person = QPerson.person;
-        return person.lastName.like(getLikePattern(searchTerm));
+        return person.lastName.toLowerCase().like(getLikePattern(searchTerm));
     }
 
     private static String getLikePattern(final String searchTerm) {
--- Spring/data-jpa/tutorial-part-five/src/test/java/net/petrikainulainen/spring/datajpa/repository/PersonPredicatesTest.java
+++ Spring/data-jpa/tutorial-part-five/src/test/java/net/petrikainulainen/spring/datajpa/repository/PersonPredicatesTest.java
@@ -1,5 +1,6 @@
 package net.petrikainulainen.spring.datajpa.repository;
 
+import com.mysema.query.types.Predicate;
 import com.mysema.query.types.expr.BooleanExpression;
 import org.junit.Test;
 
@@ -11,11 +12,11 @@ import static junit.framework.Assert.assertEquals;
 public class PersonPredicatesTest {
     
     private static final String SEARCH_TERM = "Foo";
-    private static final String EXPECTED_PREDICATE_STRING = "person.lastName like foo%";
+    private static final String EXPECTED_PREDICATE_STRING = "lower(person.lastName) like foo%";
 
     @Test
     public void lastNameLike() {
-        BooleanExpression predicate = PersonPredicates.lastNameIsLike(SEARCH_TERM);
+        Predicate predicate = PersonPredicates.lastNameIsLike(SEARCH_TERM);
         String predicateAsString = predicate.toString();
         assertEquals(EXPECTED_PREDICATE_STRING, predicateAsString);
     }
