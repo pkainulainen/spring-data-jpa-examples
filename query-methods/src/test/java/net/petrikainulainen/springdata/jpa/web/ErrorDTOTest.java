4b103821ed874560d06539758e64d2f768489fd3
Petri Kainulainen <petri.kainulainen@gmail.com>
2015-02-03 21:07:27 +0200

    Rewrite test to use NestedRunner

--- query-methods/src/test/java/net/petrikainulainen/springdata/jpa/web/ErrorDTOTest.java
+++ query-methods/src/test/java/net/petrikainulainen/springdata/jpa/web/ErrorDTOTest.java
@@ -1,46 +1,60 @@
 package net.petrikainulainen.springdata.jpa.web;
 
+import com.nitorcreations.junit.runners.NestedRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Petri Kainulainen
  */
+@RunWith(NestedRunner.class)
 public class ErrorDTOTest {
 
     private static final String CODE = "code";
     private static final String MESSAGE = "message";
 
-    @Test(expected = NullPointerException.class)
-    public void createNew_CodeIsNull_ShouldThrowException() {
-        new ErrorDTO(null, MESSAGE);
-    }
+    public class CreateNew {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void createNew_CodeIsEmpty_ShouldThrowException() {
-        new ErrorDTO("", MESSAGE);
-    }
+        public class WhenCodeIsInvalid {
+            @Test(expected = NullPointerException.class)
+            public void shouldThrowExceptionWhenCodeIsNull() {
+                new ErrorDTO(null, MESSAGE);
+            }
 
-    @Test(expected = NullPointerException.class)
-    public void createNew_MessageIsNull_ShouldThrowException() {
-        new ErrorDTO(CODE, null);
-    }
+            @Test(expected = IllegalArgumentException.class)
+            public void shouldThrowExceptionWhenCodeIsEmpty() {
+                new ErrorDTO("", MESSAGE);
+            }
+        }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void createNew_MessageIsEmpty_ShouldThrowException() {
-        new ErrorDTO(CODE, "");
-    }
+        public class WhenMessageIsInvalid {
 
-    @Test
-    public void createNew_ValidCodeAndMessageGiven_ShouldCreateNewObjectAndSetCode() {
-        ErrorDTO error = new ErrorDTO(CODE, MESSAGE);
-        assertThat(error.getCode()).isEqualTo(CODE);
-    }
+            @Test(expected = NullPointerException.class)
+            public void shouldThrowExceptionWhenMessageIsNull() {
+                new ErrorDTO(CODE, null);
+            }
 
-    @Test
-    public void createNew_ValidCodeAndMessageGiven_ShouldCreateNewObjectAndSetMessage() {
-        ErrorDTO error = new ErrorDTO(CODE, MESSAGE);
-        assertThat(error.getMessage()).isEqualTo(MESSAGE);
+            @Test(expected = IllegalArgumentException.class)
+            public void shouldThrowExceptionWhenMessageIsEmpty() {
+                new ErrorDTO(CODE, "");
+            }
+        }
+
+        public class WhenCodeAndMessageAreValid {
+
+            @Test
+            public void shouldCreateNewObjectAndSetCode() {
+                ErrorDTO error = new ErrorDTO(CODE, MESSAGE);
+                assertThat(error.getCode()).isEqualTo(CODE);
+            }
+
+            @Test
+            public void shouldCreateNewObjectAndSetMessage() {
+                ErrorDTO error = new ErrorDTO(CODE, MESSAGE);
+                assertThat(error.getMessage()).isEqualTo(MESSAGE);
+            }
+        }
     }
 }
