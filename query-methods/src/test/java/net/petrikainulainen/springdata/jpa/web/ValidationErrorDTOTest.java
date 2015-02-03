3408f22a221962cff1d5ffdd8f6a8b8c483d01ed
Petri Kainulainen <petri.kainulainen@gmail.com>
2015-02-03 21:15:49 +0200

    Rewrite test to use NestedRunner

--- query-methods/src/test/java/net/petrikainulainen/springdata/jpa/web/ValidationErrorDTOTest.java
+++ query-methods/src/test/java/net/petrikainulainen/springdata/jpa/web/ValidationErrorDTOTest.java
@@ -1,6 +1,8 @@
 package net.petrikainulainen.springdata.jpa.web;
 
+import com.nitorcreations.junit.runners.NestedRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.List;
 
@@ -10,82 +12,119 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * @author Petri Kainulainen
  */
+@RunWith(NestedRunner.class)
 public class ValidationErrorDTOTest {
 
     private static final String FIELD = "field";
     private static final String MESSAGE = "message";
 
-    @Test(expected = NullPointerException.class)
-    public void addFieldError_FieldIsNull_ShouldThrowException() {
-        ValidationErrorDTO validationErrors = new ValidationErrorDTO();
-        validationErrors.addFieldError(null, MESSAGE);
-    }
+    public class AddFieldError {
 
-    @Test
-    public void addFieldError_FieldIsNull_ShouldNotCreateNewFieldError() {
-        ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+        public class WhenFieldIsInvalid {
 
-        thrown(() -> validationErrors.addFieldError(null, MESSAGE));
+            public class WhenFieldIsNull {
 
-        assertThat(validationErrors.getFieldErrors()).isEmpty();
-    }
+                @Test(expected = NullPointerException.class)
+                public void shouldThrowException() {
+                    ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+                    validationErrors.addFieldError(null, MESSAGE);
+                }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addFieldError_FieldIsEmpty_ShouldThrowException() {
-        ValidationErrorDTO validationErrors = new ValidationErrorDTO();
-        validationErrors.addFieldError("", MESSAGE);
-    }
+                @Test
+                public void shouldNotCreateNewFieldError() {
+                    ValidationErrorDTO validationErrors = new ValidationErrorDTO();
 
-    @Test
-    public void addFieldError_FieldIsEmpty_ShouldNotCreateNewFieldError() {
-        ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+                    thrown(() -> validationErrors.addFieldError(null, MESSAGE));
 
-        thrown(() -> validationErrors.addFieldError("", MESSAGE));
+                    assertThat(validationErrors.getFieldErrors()).isEmpty();
+                }
+            }
 
-        assertThat(validationErrors.getFieldErrors()).isEmpty();
-    }
+            public class WhenFieldIsEmpty {
 
-    @Test(expected = NullPointerException.class)
-    public void addFieldError_MessageIsNull_ShouldThrowException() {
-        ValidationErrorDTO validationErrors = new ValidationErrorDTO();
-        validationErrors.addFieldError(FIELD, null);
-    }
+                @Test(expected = IllegalArgumentException.class)
+                public void shouldThrowException() {
+                    ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+                    validationErrors.addFieldError("", MESSAGE);
+                }
 
-    @Test
-    public void addFieldError_MessageIsNull_ShouldNotCreateNewFieldError() {
-        ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+                @Test
+                public void shouldNotCreateNewFieldError() {
+                    ValidationErrorDTO validationErrors = new ValidationErrorDTO();
 
-        thrown(() -> validationErrors.addFieldError(FIELD, null));
+                    thrown(() -> validationErrors.addFieldError("", MESSAGE));
 
-        assertThat(validationErrors.getFieldErrors()).isEmpty();
-    }
+                    assertThat(validationErrors.getFieldErrors()).isEmpty();
+                }
+            }
+        }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addFieldError_MessageIsEmpty_ShouldThrowException() {
-        ValidationErrorDTO validationErrors = new ValidationErrorDTO();
-        validationErrors.addFieldError(FIELD, "");
-    }
+        public class WhenMessageIsInvalid {
 
-    @Test
-    public void addFieldError_MessageIsEmpty_ShouldNotCreateNewFieldError() {
-        ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+            public class WhenMessageIsNull {
 
-        thrown(() -> validationErrors.addFieldError(FIELD, ""));
+                @Test(expected = NullPointerException.class)
+                public void shouldThrowException() {
+                    ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+                    validationErrors.addFieldError(FIELD, null);
+                }
 
-        assertThat(validationErrors.getFieldErrors()).isEmpty();
-    }
+                @Test
+                public void shouldNotCreateNewFieldError() {
+                    ValidationErrorDTO validationErrors = new ValidationErrorDTO();
 
-    @Test
-    public void addFieldError_LegalFieldAndMessageGiven_ShouldCreateNewFieldError() {
-        ValidationErrorDTO validationErrors = new ValidationErrorDTO();
-        validationErrors.addFieldError(FIELD, MESSAGE);
+                    thrown(() -> validationErrors.addFieldError(FIELD, null));
 
-        List<FieldErrorDTO> fieldErrors = validationErrors.getFieldErrors();
-        assertThat(fieldErrors).hasSize(1);
+                    assertThat(validationErrors.getFieldErrors()).isEmpty();
+                }
+            }
 
-        FieldErrorDTO fieldError = fieldErrors.iterator().next();
+            public class WhenMessageIsEmpty {
 
-        assertThat(fieldError.getField()).isEqualTo(FIELD);
-        assertThat(fieldError.getMessage()).isEqualTo(MESSAGE);
+                @Test(expected = IllegalArgumentException.class)
+                public void shouldThrowException() {
+                    ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+                    validationErrors.addFieldError(FIELD, "");
+                }
+
+                @Test
+                public void shouldNotCreateNewFieldError() {
+                    ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+
+                    thrown(() -> validationErrors.addFieldError(FIELD, ""));
+
+                    assertThat(validationErrors.getFieldErrors()).isEmpty();
+                }
+            }
+        }
+
+        public class WhenFieldAndMessageAreValid {
+
+            @Test
+            public void shouldCreateNewFieldErrorAndSetField() {
+                ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+                validationErrors.addFieldError(FIELD, MESSAGE);
+
+                List<FieldErrorDTO> fieldErrors = validationErrors.getFieldErrors();
+                assertThat(fieldErrors).hasSize(1);
+
+                FieldErrorDTO fieldError = fieldErrors.iterator().next();
+
+                assertThat(fieldError.getField()).isEqualTo(FIELD);
+            }
+
+            @Test
+            public void shouldCreateNewFieldErrorAndSetMessage() {
+                ValidationErrorDTO validationErrors = new ValidationErrorDTO();
+                validationErrors.addFieldError(FIELD, MESSAGE);
+
+                List<FieldErrorDTO> fieldErrors = validationErrors.getFieldErrors();
+                assertThat(fieldErrors).hasSize(1);
+
+                FieldErrorDTO fieldError = fieldErrors.iterator().next();
+
+                assertThat(fieldError.getMessage()).isEqualTo(MESSAGE);
+            }
+        }
     }
 }
