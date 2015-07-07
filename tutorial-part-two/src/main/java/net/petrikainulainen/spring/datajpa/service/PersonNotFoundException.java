ec17b11528a094d21ee804de590e43c770837ca3
Petri Kainulainen <petri.kainulainen@gmail.com>
2015-07-07 21:32:21 +0300

    Removed old example projects

--- tutorial-part-two/src/main/java/net/petrikainulainen/spring/datajpa/service/PersonNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package net.petrikainulainen.spring.datajpa.service;
-
-/**
- * This exception is thrown if the wanted person is not found.
- * @author Petri Kainulainen
- */
-public class PersonNotFoundException extends Exception {
-}
