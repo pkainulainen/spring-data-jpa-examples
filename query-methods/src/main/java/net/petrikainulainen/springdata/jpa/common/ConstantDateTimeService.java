a21f4f5979d1bab25888f0548717ae2fe93fb428
Petri Kainulainen <petri.kainulainen@gmail.com>
2015-07-19 15:47:24 +0300

    Ensure that constant datetime contains systems zone offset and zone id.

--- query-methods/src/main/java/net/petrikainulainen/springdata/jpa/common/ConstantDateTimeService.java
+++ query-methods/src/main/java/net/petrikainulainen/springdata/jpa/common/ConstantDateTimeService.java
@@ -3,6 +3,7 @@ package net.petrikainulainen.springdata.jpa.common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -15,11 +16,25 @@ import java.time.format.DateTimeFormatter;
  */
 public class ConstantDateTimeService implements DateTimeService {
 
-    public static final String CURRENT_DATE_AND_TIME = "2015-07-19T12:52:28+03:00[Europe/Helsinki]";
+    public static final String CURRENT_DATE_AND_TIME = getConstantDateAndTime();
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_ZONED_DATE_TIME;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConstantDateTimeService.class);
+
+    private static String getConstantDateAndTime() {
+        return "2015-07-19T12:52:28" +
+                getSystemZoneOffset() +
+                getSystemZoneId();
+    }
+
+    private static String getSystemZoneOffset() {
+        return ZonedDateTime.now().getOffset().toString();
+    }
+
+    private static String getSystemZoneId() {
+        return "[" + ZoneId.systemDefault().toString() + "]";
+    }
 
     @Override
     public ZonedDateTime getCurrentDateAndTime() {
