9d0088a3a77cc7ee491cb4b6566f6ce3bbef2821
Petri Kainulainen <petri.kainulainen@gmail.com>
2015-09-06 15:10:53 +0300

    Remove unnecessary DirtiesContextTestExecutionListener

--- criteria-api/src/integration-test/java/net/petrikainulainen/springdata/jpa/todo/ITTodoRepositoryTest.java
+++ criteria-api/src/integration-test/java/net/petrikainulainen/springdata/jpa/todo/ITTodoRepositoryTest.java
@@ -15,7 +15,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -32,7 +31,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ContextConfiguration(classes = {ExampleApplicationContext.class})
 @DbUnitConfiguration(dataSetLoader = ColumnSensingReplacementDataSetLoader.class)
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
         TransactionalTestExecutionListener.class,
         DbUnitTestExecutionListener.class})
 @WebAppConfiguration
